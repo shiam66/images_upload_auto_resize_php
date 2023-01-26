5b849fce0b22ad1e5f7a9618003bc0bab802b19e
Md. Shiam Hossain <shiamjpi161@gmail.com>
2023-01-26 14:30:33 +0600

    first upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP Crop Images then Upload</title>
+</head>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file" required>
+    <input type="submit" name="submit"/>
+</form>
+
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,51 @@
+<?php
+
+    $file = $_FILES['file']['tmp_name'];
+
+    // $filetype = $_FILES['file']['size'];
+    // print_r($filetype);
+    // die();
+
+    list($width,$height) = getimagesize($file);
+    
+    $nwidth = 312;   // $width/4; ratio divide 4 or 312 width
+    $nheight = 350; // $height/4; ratio divide 4 or 350 height
+
+    $newimage=imagecreatetruecolor($nwidth,$nheight);
+
+    if($_FILES['file']['type'] == 'image/jpg'){
+        $source = imagecreatefromjpg($file);
+        imagecopyresized($newimage,$source,0,0,0,0,$nwidth,$nheight,$width,$height);
+        $file_name = time().'.jpg';
+        imagejpg($newimage,'upload/'.$file_name);
+
+        header("location:index.php?msg=1");
+    }
+    elseif($_FILES['file']['type'] == 'image/png'){
+        $source = imagecreatefrompng($file);
+        imagecopyresized($newimage,$source,0,0,0,0,$nwidth,$nheight,$width,$height);
+        $file_name = time().'.png';
+        imagepng($newimage,'upload/'.$file_name);
+        
+        header("location:index.php?msg=1");
+    }
+    elseif($_FILES['file']['type'] == 'image/gif'){
+        $source = imagecreatefromgif($file);
+        imagecopyresized($newimage,$source,0,0,0,0,$nwidth,$nheight,$width,$height);
+        $file_name = time().'.gif';
+        imagegif($newimage,'upload/'.$file_name);
+
+        header("location:index.php?msg=1");
+    }
+    elseif($_FILES['file']['type'] == 'image/jpeg'){
+        $source = imagecreatefromjpeg($file);
+        imagecopyresized($newimage,$source,0,0,0,0,$nwidth,$nheight,$width,$height);
+        $file_name = time().'.jpg';
+        imagejpeg($newimage,'upload/'.$file_name);
+
+        header("location:index.php?msg=1");
+    }
+    else{
+        echo "Please select only jpg, jpeg, png and gif image";
+    }
+?>
